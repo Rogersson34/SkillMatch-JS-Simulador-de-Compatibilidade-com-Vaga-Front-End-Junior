ded88ed89ad9adb330301e44d039cf00c58c7148
Roger <rogersson2024@gmail.com>
2026-05-17 18:54:26 -0300

    Organizando código

--- SkillMatch-JS/skillmatch.js
+++ SkillMatch-JS/skillmatch.js
@@ -1,60 +1,16 @@
 //Perfis do candidato
 const candidatos = [
-    {
-        nome: "João Silva",
-        vaga: "Desenvolvedor Front-end React",
-        habilidades: ["HTML", "CSS", "JavaScript", "React", "GitHub", "Trello"],
-        experienciaMeses: 12
-    },
-
-    {
-        nome: "Ana Julia",
-        vaga: "Desenvolvedor Front-end Junior",
-        habilidades: ["HTML", "CSS", "JavaScript", "GitHub"],
-        experienciaMeses: 6
-    },
-
-    {
-        nome: "Paulo César",
-        vaga: "Desenvolvedor Front-end React",
-        habilidades: ["HTML", "CSS", "JavaScript", "React", "GitHub", "TypeScript", "Trello"],
-        experienciaMeses: 18
-    },
-    {
-        nome: "Maria Clara",
-        vaga: "Desenvolvedor Front-end Junior",
-        habilidades: ["HTML", "CSS", "GitHub", "Trello"],
-        experienciaMeses: 10
-    }
+    { nome: "João Silva", vaga: "Desenvolvedor Front-end React", habilidades: ["HTML", "CSS", "JavaScript", "React", "GitHub", "Trello"], experienciaMeses: 12 },
+    { nome: "Ana Julia", vaga: "Desenvolvedor Front-end Junior", habilidades: ["HTML", "CSS", "JavaScript", "GitHub"], experienciaMeses: 6 },
+    { nome: "Paulo César", vaga: "Desenvolvedor Front-end React", habilidades: ["HTML", "CSS", "JavaScript", "React", "GitHub", "TypeScript", "Trello"], experienciaMeses: 18 },
+    { nome: "Maria Clara", vaga: "Desenvolvedor Front-end Junior", habilidades: ["HTML", "CSS", "GitHub", "Trello"], experienciaMeses: 10 }
 ];
 
 //Listar as vagas
 const vagas = [
-    {
-        id: 1,
-        empresa: "StarsRH",
-        cargo: "Programador Front-End Júnior",
-        requisitos: ["JavaScript", "GitHub", "React", "Node"],
-        salario: 3000,
-        modalidade: "Híbrido"
-    },
-
-    {
-        id: 2,
-        empresa: "AgTech",
-        cargo: "Desenvolvedor JavaScript, React",
-        requisitos: ["JavaScript", "React", "Node", "TypeScript", "GitHub", "Trello"],
-        salario: 3200,
-        modalidade: "Remoto"
-    },
-    {
-        id: 3,
-        empresa: "NovosTalentos",
-        cargo: "Programador Front-End Júnior",
-        requisitos: ["JavaScript", "GitHub", "Trello"],
-        salario: 1800,
-        modalidade: "Presencial"
-    },
+    { id: 1, empresa: "StarsRH", cargo: "Programador Front-End Júnior", requisitos: ["JavaScript", "GitHub", "React", "Node"], salario: 3000, modalidade: "Híbrido" },
+    { id: 2, empresa: "AgTech", cargo: "Desenvolvedor JavaScript, React", requisitos: ["JavaScript", "React", "Node", "TypeScript", "GitHub", "Trello"], salario: 3200, modalidade: "Remoto" },
+    { id: 3, empresa: "NovosTalentos", cargo: "Programador Front-End Júnior", requisitos: ["JavaScript", "GitHub", "Trello"], salario: 1800, modalidade: "Presencial" },
 
 ];
 
@@ -82,19 +38,20 @@ candidatos.forEach(candidato => {
         let classificacao = "";
 
         if (compatibilidade === 100) {
-            classificacao = "Match Perfeito! 🚀";
-        } else if (compatibilidade >= 70) {
+            classificacao = "Match Perfeito, candidato atende todos os requisitos! 🚀";
+        } else if (compatibilidade >= 80) {
             classificacao = "Alta Compatibilidade (Forte candidato) ✅";
-        } else if (compatibilidade >= 40) {
-            classificacao = "Média Compatibilidade (Precisa estudar alguns requisitos) ⚠️";
+        } else if (compatibilidade >= 50) {
+            classificacao = "Média Compatibilidade (Candidato atende alguns requisitos) ⚠️";
         } else {
             classificacao = "Baixa Compatibilidade (Não atende aos requisitos da vaga) ❌";
         }
         // ------------------------------------------
 
         console.log(`Vaga: ${vaga.empresa} - ${vaga.cargo}`);
-        console.log(`Compatibilidade: ${compatibilidade}% - [${classificacao}]`);
-        console.log(`Faltantes: ${faltantes.join(", ") || "Nenhum"}`);
-        console.log("—");
+        console.log(`Compatibilidade: ${compatibilidade}% - [${classificacao}]`);//Encontrar a vaga com maior compatibilidade
+        console.log(`Habilidades Faltantes: ${faltantes.join(", ") || "Nenhum"}`);//Listar Habilidades faltantes
+        console.log(`Recomendação de Estudo: ${vaga.requisitos.join(", ") || "Nenhum"}`);//Gerar uma recomendação de estudo
     });
 });
+//Uso de Arrays
