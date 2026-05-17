644ab35ce18e44e00928dd3498b5b9ed99b27622
Roger <rogersson2024@gmail.com>
2026-05-17 13:18:07 -0300

    Dados-candidatos-vagas

--- SkillMatch-JS/skillmatch.js
+++ SkillMatch-JS/skillmatch.js
@@ -6,3 +6,47 @@ const candidato = {
     experieciaMeses: 3
 };
 
+const candidato = {
+    nome: "Ana Julia",
+    vaga: "Desenvolvedor Front-end",
+    habilidades: ["HTML", "CSS", "JavaScript", "GitHub"],
+    experieciaMeses: 12
+};
+
+const candidato = {
+    nome: "Paulo César",
+    vaga: "Desenvolvedor Front-end",
+    habilidades: ["HTML", "CSS", "JavaScript", "React", "GitHub", "Trello"],
+    experieciaMeses: 6
+};
+
+//Listar as vagas
+
+const vagas = [
+    {
+        id: 1,
+        empresa: "StarsRH",
+        cargo: "Programador Front-End Júnior",
+        requisitos: ["JavaScript", "GitHub", "React", "Node"],
+        salario: 3000,
+        modalidade: "Híbrido"
+    },
+
+    {
+        id: 2,
+        empresa: "AgTech",
+        cargo: "Desenvolvedor JavaScript, React",
+        requisitos: ["JavaScript", "React", "Node", "TypeScript", "GitHub", "Trello"],
+        salario: 3200,
+        modalidade: "Remoto"
+    },
+    {
+        id: 3,
+        empresa: "NovosTalentos",
+        cargo: "Programador Front-End Júnior",
+        requisitos: ["JavaScript", "GitHub", "Trello"],
+        salario: 1800,
+        modalidade: "Presencial"
+    },
+
+]; 
